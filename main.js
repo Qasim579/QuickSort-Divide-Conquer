dc40f04257b5d07814e561d29b8b45dbfe8e6164
Qasim579 <kqasim579@gmail.com>
2021-05-31 20:58:06 +0500

    init

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-swap = (left, right) => {
-    var temp = left
-    left = right;
-    right = temp;
+var items = [5,3,7,6,2,9];
+ swap = (items, leftIndex, rightIndex)=>{
+    var temp = items[leftIndex];
+    items[leftIndex] = items[rightIndex];
+    items[rightIndex] = temp;
 }
 
 partition = (items, left, right) => {
@@ -18,31 +19,26 @@ partition = (items, left, right) => {
             j--;
         }
         if (i <= j) {
-            swap(i, j);
+            swap(items,i, j);
             i++;
             j--;
         }
     }
     return i;
 }
-partition = (items, left, right) => {
-    var pivot = items[Math.floor((right + left) / 2)];
-    i = left;
-    j = right;
-
-    while (i <= j) {
-
-        while (items[i] < pivot) {
-            i++;
+quickSort = (items,left,right)=>{
+    var index;
+    if(items.length >1){
+        index = partition(items,left,right);
+        if(left<index-1){
+            quickSort(items,left,index-1);
         }
-        while (items[j] > pivot) {
-            j--;
-        }
-        if (i <= j) {
-            swap(i, j);
-            i++;
-            j--;
+        if(index<right){
+            quickSort(items,index,right);
         }
     }
-    return i;
+    return items;
+    
 }
+var result = quickSort(items,0,items.length-1);
+console.log(result);
